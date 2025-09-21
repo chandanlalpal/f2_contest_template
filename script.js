3456edb9e0ef0c1cce6efd2979aec7a89ff54cfd
chandan lal <cpal87436@gmail.com>
2025-09-21 08:30:31 +0530

    completed

--- script.js
+++ script.js
@@ -8,20 +8,50 @@ let arr = [
 
 function PrintDeveloperbyMap() {
   //Write your code here , just console.log
+    const developers = arr.map((person) => {
+        if (person.profession === "developer") {
+            console.log(person);
+        }
+    });
 }
 
 function PrintDeveloperbyForEach() {
-  //Write your code here , just console.log
+  arr.forEach((person) => {
+      if (person.profession === "developer") {
+          console.log(person);
+      }
+  });
+
 }
 
 function addData() {
+   function addData() {
+   let newUser = { id: 4, name: "susan", age: "20", profession: "intern" };
+    arr.push(newUser);
+     console.log(arr);
+
+   }
+
   //Write your code here, just console.log
+
+
 }
 
 function removeAdmin() {
   //Write your code here, just console.log
+  function removeAdmin() {
+  arr=arr.filter((person) => person.profession !== "admin");
+    console.log(arr);
+  }
 }
 
 function concatenateArray() {
+ let newArray = [
+
+ {id:5,name:"sam",age:"23",profession:"developer"},
+ {id:6,name:"sara",age:"24",profession:"developer"},
+ ];
+ let concatendate =arr.concat(newArray);}
+ console.log(concatendate);
   //Write your code here, just console.log
 }
